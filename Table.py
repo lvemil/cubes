b1d8f2cea534fedf636e3ca2f53950555877f07c
Emil Lima <elima@pop-os.localdomain>
2019-10-27 13:51:35 -0600

    now cubes arrangement is vertically and horizontally centered in the table

--- Table.py
+++ Table.py
@@ -21,24 +21,27 @@ class Table:
         vs = str(value)
         max_digit = max([int(d) for d in vs])
         return math.ceil(max_digit / cols_by_digit)
-                
+    
+    def ValueWidth(self, value: int, cols_by_digit = 1):
+        return sum([(x if x < cols_by_digit else cols_by_digit) for x in [int(x) for x in str(value)]])
+    
     def SetValue(self, value: int, cols_by_digit = 1):
-        initial_row =  int(self.rows / 2 - self.ValueHeigth(value, cols_by_digit) / 2)
-        
+        initial_row = int(self.rows / 2 - self.ValueHeigth(value, cols_by_digit) / 2)
+        initial_col = int(self.cols / 2 + self.ValueWidth(value, cols_by_digit) / 2)
         vs = str(value)
         p = len(vs)                
         col = 0
                        
-        for d in reversed(vs):                  
+        for d in [int(x) for x in reversed(vs)]:                  
             if p == len(vs):
-                col = (p * cols_by_digit - cols_by_digit + 1)
+                col = initial_col - (d if d < cols_by_digit else cols_by_digit)
             else:
-                if int(d) < cols_by_digit:
-                    col -= int(d)
+                if d < cols_by_digit:
+                    col -= d
                 else:
                     col -= cols_by_digit
 
-            self.SetDigit(int(d), p, len(vs), cols_by_digit, row = initial_row, col = col)
+            self.SetDigit(d, p, len(vs), cols_by_digit, row = initial_row, col = col)
             p -= 1
         
     def SetDigit(self, d: int, p: int):
